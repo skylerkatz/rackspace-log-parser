857f66a32b406c7c61d6edb1feacd3d7bf7d8077
Skyler Katz <skyler@monkdevelopment.com>
2018-11-03 14:15:49 -0700

    Initial Parser

--- src/RackspaceLogParser.php
+++ src/RackspaceLogParser.php
@@ -5,21 +5,140 @@ namespace SkylerKatz\RackspaceLogParser;
 class RackspaceLogParser
 {
     /**
-     * Create a new RackspaceLogParser Instance.
+     * Rexeg Pattern to match Rackspace Cloud Files Logs
      */
-    public function __construct()
+    const PATTERN = '/^([^ ]+) ([^ ]+) ([^ ]+) (\[[^\]]+\]) "(.*) (.*) (.*)" ([0-9\-]+) ([0-9\-]+) "(.*)" "(.*)/m';
+
+    /**
+     * Max Buffer size for reading log file
+     */
+    const BUFFER_SIZE = 4096;
+
+    /**
+     * The path to the log file
+     *
+     * @var string
+     */
+    protected $path;
+
+    /**
+     * The log file in memory
+     *
+     * @var string
+     */
+    protected $log;
+
+
+    /**
+     * Create a new RackspaceLogParser Instance.
+     *
+     * @param string $path
+     */
+    public function __construct($path)
     {
-        // constructor body
+        $this->path = $path;
+        $this->processLog();
     }
 
     /**
-     * Friendly welcome.
+     * Parse a log file into an arrray of LogItems
      *
-     * @param string $phrase Phrase to return
-     * @return string Returns the phrase passed in
+     * @return array
      */
-    public function echoPhrase($phrase)
+    public function parse()
     {
-        return $phrase;
+        $entries = [];
+
+        foreach ($this->getLogRows() as $row) {
+            $entry = $this->parseRow($row);
+            $entries[] = $this->createLogItem($entry);
+        }
+
+        return $entries;
+    }
+
+    /**
+     * read a log file into memory
+     *
+     * @return void
+     */
+    private function processLog()
+    {
+        // Open our file in binary mode
+        $file = gzopen($this->path, 'rb');
+
+        // Keep repeating until the end of the input file
+        $log = '';
+        while (!gzeof($file)) {
+            $log .= gzread($file, self::BUFFER_SIZE);
+        }
+
+        // File is done, close files
+        gzclose($file);
+
+        $this->log = $log;
+
+        return $this;
+    }
+
+    /**
+     * Get the rows of a log
+     *
+     * @return array
+     */
+    private function getLogRows()
+    {
+        return array_filter(explode(PHP_EOL, $this->log));
+    }
+
+    /**
+     * Parse a row into its component parts
+     *
+     * @param string $row
+     * @return array
+     */
+    private function parseRow($row)
+    {
+        preg_match(self::PATTERN, $row, $entry);
+
+        return $entry;
+    }
+
+    /**
+     * Create a Log Item from a parsed log row
+     *
+     * @param array $entry
+     * @return LogItem
+     */
+    private function createLogItem($entry)
+    {
+        $logItem = new LogItem();
+        $logItem->raw = $entry[0];
+        $logItem->clientIp = $entry[1];
+        $logItem->accessDate = $this->parseDate($entry[4]);
+        $logItem->method = $entry[5];
+        $logItem->request = $entry[6];
+        $logItem->httpVersion = $entry[7];
+        $logItem->returnCode = intval($entry[8]);
+        $logItem->bytesSent = intval($entry[9]);
+
+        $logItem->referrer = $entry[10] == '-' ? null : $entry[10];
+        $logItem->userAgent = $entry[11];
+
+        return $logItem;
+    }
+
+    /**
+     * Parse a log date
+     *
+     * @param string $date
+     * @return \Carbon\Carbon
+     */
+    private function parseDate($date)
+    {
+        $accessDate = str_replace('[', '', $date);
+        $accessDate = str_replace(']', '', $accessDate);
+        $accessDate = explode(' ', $accessDate);
+        return \Carbon\Carbon::createFromFormat('d/m/Y:H:i:s', $accessDate[0], new \DateTimeZone('UTC'));
     }
 }
---
+++ tests/RackspaceLogParserTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace SkylerKatz\RackspaceLogParser\Tests;
+
+use PHPUnit\Framework\TestCase;
+use SkylerKatz\RackspaceLogParser\RackspaceLogParser;
+
+class RackspaceLogParserTest extends TestCase
+{
+    /** @test */
+    public function it_can_parse_a_log_file()
+    {
+        $path = __DIR__ . '/stubs/cloudfiles.log.stub';
+
+        $parser = new RackspaceLogParser($path);
+        $result = $parser->parse();
+        $this->assertCount(5, $result);
+
+        $this->assertEquals('173.203.44.122 - - [15/07/2014:20:52:25 +0000] "GET /5142b6e5e57f760d7ff4-c591437fc634f2a98934b7738b8b8571.r93.cf1.rackcdn.com/image1.png HTTP/1.1" 304 277 "http://www.rackspace.com/" "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0. 50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3; .NET4.0C; .NET4.0E; MS-RTC LM 8; Microsoft Outlook 14.0. 7109; ms-office; MSOffice 14)"', $result[0]->raw);
+        $this->assertEquals('173.203.44.122', $result[0]->clientIp);
+        $this->assertEquals('GET', $result[0]->method);
+        $this->assertEquals('/5142b6e5e57f760d7ff4-c591437fc634f2a98934b7738b8b8571.r93.cf1.rackcdn.com/image1.png', $result[0]->request);
+        $this->assertEquals('HTTP/1.1', $result[0]->httpVersion);
+        $this->assertEquals('304', $result[0]->returnCode);
+        $this->assertEquals('277', $result[0]->bytesSent);
+        $this->assertEquals('http://www.rackspace.com/', $result[0]->referrer);
+        $this->assertEquals('Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0. 50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3; .NET4.0C; .NET4.0E; MS-RTC LM 8; Microsoft Outlook 14.0. 7109; ms-office; MSOffice 14)"', $result[0]->userAgent);
+    }
+}
